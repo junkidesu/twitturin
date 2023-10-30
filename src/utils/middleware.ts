f38765c0bb67fa62633234c5529cc6acc77d402f
Anwar Sh <alvaro.sh03@gmail.com>
2023-10-31 02:37:55 +0500

    Refactor requireAuthentication; implement requireAuthor

--- src/utils/middleware.ts
+++ src/utils/middleware.ts
@@ -1,8 +1,10 @@
 import { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
-import { AuthError } from "../types";
+import { AuthError, IUser, NotFoundError } from "../types";
+import { Types } from "mongoose";
 import env from "./config";
 import User from "../models/user";
+import Tweet from "../models/tweet";
 
 const parseToken = (text: string): string => {
   if (!text || !text.startsWith("Bearer "))
@@ -34,6 +36,53 @@ export const userExtractor = async (
     const decodedToken = jwt.verify(token, env.SECRET);
     const user = await extractUser(decodedToken);
     req.user = user;
+    next();
+  } catch (error) {
+    next(error);
+  }
+};
+
+const checkAuthentication = (req: Request) => {
+  if (!req.user) throw new AuthError("authentication required");
+};
+
+export const requireAuthentication = (
+  req: Request,
+  _res: Response,
+  next: NextFunction
+) => {
+  try {
+    checkAuthentication(req);
+
+    next();
+  } catch (error) {
+    next(error);
+  }
+};
+
+const checkAuthor = async (
+  user: IUser & { _id: Types.ObjectId },
+  id: string
+) => {
+  const tweet = await Tweet.findById(id);
+
+  if (!tweet) throw new NotFoundError("tweet not found");
+
+  const author = tweet.author.toString();
+  const userId = user._id.toString();
+
+  if (userId !== author) throw new AuthError("need to be author of tweet");
+};
+
+export const requireAuthor = async (
+  req: Request,
+  _res: Response,
+  next: NextFunction
+) => {
+  if (!("id" in req.params) || !req.user) return next();
+
+  try {
+    await checkAuthor(req.user, req.params.id);
     next();
   } catch (error) {
     next(error);
