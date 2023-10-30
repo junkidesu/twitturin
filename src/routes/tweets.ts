e5ded87ee4e545d193782a8d01d72a4b385bbb88
Anwar Sh <alvaro.sh03@gmail.com>
2023-10-30 21:25:58 +0500

    Implement POST /api/tweets endpoint

--- src/routes/tweets.ts
+++ src/routes/tweets.ts
@@ -1,11 +1,29 @@
 import express from "express";
 import tweetsService from "../services/tweetsService";
+import { AuthError } from "../types";
 
 const router = express.Router();
 
 router.get("/", async (_req, res) => {
   const tweets = await tweetsService.getAllTweets();
   return res.json(tweets);
+});
+
+router.post("/", async (req, res, next) => {
+  try {
+    if (!req.user) throw new AuthError("must be authenticated to post");
+
+    const { content } = req.body as { content: string };
+
+    const addedTweet = await tweetsService.addTweet({
+      content,
+      author: req.user._id.toString(),
+    });
+
+    res.status(201).send(addedTweet);
+  } catch (error) {
+    next(error);
+  }
 });
 
 export default router;
