801339d2c4915ba0f92a6d9374279d162d35ba43
Anwar Sh <alvaro.sh03@gmail.com>
2023-10-30 23:02:47 +0500

    Define "tweets" virtual property for userSchema

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,41 +1,54 @@
+/* eslint-disable no-self-assign */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { Schema, model } from "mongoose";
 import { IUser } from "../types";
 
-const userSchema = new Schema<IUser>({
-  username: {
-    type: String,
-    required: true,
+const userSchema = new Schema<IUser>(
+  {
+    username: {
+      type: String,
+      required: true,
+    },
+    fullName: String,
+    passwordHash: {
+      type: String,
+      required: true,
+    },
+    major: {
+      type: String,
+      enum: ["SE", "BM", "IT", "ME", "CIE", "AD", "AE"],
+      required: true,
+    },
+    studentId: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    age: Number,
+    country: String,
   },
-  fullName: String,
-  passwordHash: {
-    type: String,
-    required: true,
-  },
-  major: {
-    type: String,
-    enum: ["SE", "BM", "IT", "ME", "CIE", "AD", "AE"],
-    required: true,
-  },
-  studentId: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-  },
-  age: Number,
-  country: String,
-});
+  {
+    toObject: { virtuals: true },
+    toJSON: {
+      virtuals: true,
+      transform: (_document, returnedObject) => {
+        delete returnedObject.passwordHash;
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
+        returnedObject.id = returnedObject._id.toString();
+        delete returnedObject._id;
+        delete returnedObject.__v;
+      },
+    },
+  }
+);
 
-userSchema.set("toJSON", {
-  transform: (_document, returnedObject) => {
-    delete returnedObject.passwordHash;
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
-  },
+userSchema.virtual("tweets", {
+  ref: "Tweet",
+  localField: "_id",
+  foreignField: "author",
 });
 
 const User = model<IUser>("User", userSchema);
