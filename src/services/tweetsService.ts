8364ffebf5ccacfe27d077ad138db90212110f64
Anwar Sh <alvaro.sh03@gmail.com>
2023-10-30 19:47:16 +0500

    Implement service to add a tweet

--- src/services/tweetsService.ts
+++ src/services/tweetsService.ts
@@ -1,4 +1,6 @@
 import Tweet from "../models/tweet";
+import User from "../models/user";
+import { NewTweet, NotFoundError } from "../types";
 
 const getAllTweets = async () => {
   const tweets = await Tweet.find({});
@@ -6,4 +8,17 @@ const getAllTweets = async () => {
   return tweets;
 };
 
-export default { getAllTweets };
+const addTweet = async ({ content, author }: NewTweet) => {
+  const foundAuthor = await User.findById(author);
+
+  if (!foundAuthor) throw new NotFoundError("user not found");
+
+  const addedTweet = await new Tweet({
+    content,
+    author: foundAuthor._id,
+  }).save();
+
+  return addedTweet;
+};
+
+export default { getAllTweets, addTweet };
--- src/types.ts
+++ src/types.ts
@@ -38,6 +38,11 @@ export interface ITweet {
   author: Types.ObjectId;
 }
 
+export interface NewTweet {
+  content: string;
+  author: string;
+}
+
 export interface TokenData {
   token: string;
   id: Types.ObjectId;
