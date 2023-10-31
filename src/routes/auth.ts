b1cfa66c4e7a9d12a356e1b3736ad241d54612e4
Anwar Sh <alvaro.sh03@gmail.com>
2023-11-01 02:48:00 +0500

    Document the /auth/api endpoint

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -4,6 +4,40 @@ import { toCredentials } from "../utils/parsers";
 
 const router = express.Router();
 
+/**
+ * @openapi
+ * /auth:
+ *   post:
+ *     summary: Authenticate with student ID and password.
+ *     description: Authenticate a user with student ID and password. The authentication scheme is bearer token (JWT).
+ *     tags: [auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/AuthData'
+ *     responses:
+ *       200:
+ *         description: JWT and user data.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/TokenData'
+ *       400:
+ *         description: invalid request body
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/responses/Error'
+ *
+ *       401:
+ *         description: incorrect studentId or password
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/responses/Error'
+ */
 router.post("/", async (req, res, next) => {
   try {
     const credentials = toCredentials(req.body);
