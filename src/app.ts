d553cd7481fc39ccea5bf290801928e34892bc73
Anwar Sh <alvaro.sh03@gmail.com>
2023-10-31 14:10:16 +0500

    Install and setup cors

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import mongoose from "mongoose";
+import cors from "cors";
 import env from "./utils/config";
 import authRouter from "./routes/auth";
 import usersRouter from "./routes/users";
@@ -16,6 +17,7 @@ mongoose
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.get("/ping", (_req, res) => {
