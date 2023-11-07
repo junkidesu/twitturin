8f945571017e9adb8acec220dab668db05a073f2
Anwar Sh <alvaro.sh03@gmail.com>
2023-11-08 03:27:14 +0500

    Add sample data to the client

---
+++ client/src/types.ts
@@ -0,0 +1,44 @@
+export enum Major {
+  Se = "SE",
+  Bm = "BM",
+  It = "IT",
+  Me = "ME",
+  Cie = "CIE",
+  Ad = "AD",
+  Ae = "AE",
+}
+
+export interface User extends BaseUser {
+  id: string;
+  username: string;
+  studentId: string;
+  fullName?: string;
+  major: Major;
+  email: string;
+  country?: string;
+  tweets: BaseTweet[];
+}
+
+export interface BaseUser {
+  id: string;
+  username: string;
+  studentId: string;
+  fullName?: string;
+  major: Major;
+  email: string;
+  country?: string;
+  age?: number;
+}
+
+export interface BaseTweet {
+  id: string;
+  likes: number;
+  content: string;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface Tweet extends BaseTweet {
+  author: BaseUser;
+  likedBy: BaseUser[];
+}
