2ab1ad4b9d597fc6594e3d2a31587d8f25e00b2f
Anwar Sh <alvaro.sh03@gmail.com>
2023-11-08 03:34:26 +0500

    Define initial global style

---
+++ client/src/theme.tsx
@@ -0,0 +1,11 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+    body {
+        padding: 0;
+        margin: 0;
+        font-family: sans-serif;
+    }
+`;
+
+export default GlobalStyle;
