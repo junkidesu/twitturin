577441881b66c5e3824a5564bdc82e47c18cb392
Anwar Sh <alvaro.sh03@gmail.com>
2023-11-08 03:41:33 +0500

    Add initial header

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,33 @@
+import styled from "styled-components";
+import GlobalStyle from "./theme";
+
+const Header = styled.header`
+  position: sticky;
+  top: 0;
+  display: flex;
+  justify-items: center;
+  align-items: center;
+  justify-content: center;
+  background: linear-gradient(45deg, turquoise, teal, slategrey);
+  padding: 1em;
+`;
+
+const LogoText = styled.p`
+  color: #eeeeee;
+  margin: 0;
+  font-weight: bold;
+  font-size: 2em;
+`;
+
 const App = () => {
-  return <div>This is a web client of TwitturIn.</div>;
+  return (
+    <div>
+      <GlobalStyle />
+      <Header>
+        <LogoText>Twitturin</LogoText>
+      </Header>
+    </div>
+  );
 };
 
 export default App;
--- client/src/theme.tsx
+++ client/src/theme.tsx
@@ -2,6 +2,7 @@ import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
     body {
+        position: relative;
         padding: 0;
         margin: 0;
         font-family: sans-serif;
