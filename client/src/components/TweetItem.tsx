177d6d4bc8b7fb1735c7618c56350103f8504ab0
Anwar Sh <alvaro.sh03@gmail.com>
2023-11-08 16:54:58 +0500

    Implement tweet bottom button bar

--- client/src/components/TweetItem.tsx
+++ client/src/components/TweetItem.tsx
@@ -1,6 +1,14 @@
 import styled from "styled-components";
 import { Tweet } from "../types";
 import emptyProfilePicture from "../assets/empty-profile-picture.png";
+import emptyHeart from "../assets/heart.svg";
+import repliesIcon from "../assets/replies.svg";
+import shareIcon from "../assets/share.svg";
+
+const Icon = styled.img`
+  width: 1.2em;
+  height: 1.2em;
+`;
 
 const TweetContainer = styled.div`
   display: flex;
@@ -14,7 +22,6 @@ const AuthorInfo = styled.div`
   display: flex;
   align-items: center;
   gap: 0.5em;
-  margin-bottom: 0.5em;
 `;
 
 const FullName = styled.div`
@@ -32,7 +39,25 @@ const ProfilePicture = styled.img`
   border-radius: 10em;
 `;
 
+const TweetButtons = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.5em;
+`;
+
+const TweetButton = styled.button`
+  display: flex;
+  align-items: center;
+  gap: 0.5em;
+  background-color: transparent;
+  border: none;
+  color: #555555;
+`;
+
 const TweetBody = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.5em;
   font-size: 1em;
   padding-left: 1em;
 `;
@@ -48,7 +73,24 @@ const TweetItem = ({ tweet }: { tweet: Tweet }) => (
         <Username>@{tweet.author.username}</Username>
       </AuthorInfo>
 
-      {tweet.content}
+      <div>{tweet.content}</div>
+
+      <TweetButtons>
+        <TweetButton>
+          <Icon src={emptyHeart} />
+          <span>{tweet.likes}</span>
+        </TweetButton>
+
+        <TweetButton>
+          <Icon src={shareIcon} />
+          <span>0</span>
+        </TweetButton>
+
+        <TweetButton>
+          <Icon src={repliesIcon} />
+          <span>0</span>
+        </TweetButton>
+      </TweetButtons>
     </TweetBody>
   </TweetContainer>
 );
