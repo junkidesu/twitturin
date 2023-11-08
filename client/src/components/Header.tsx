2513843a0d774999ff0337289fdd4134b6793406
Anwar Sh <alvaro.sh03@gmail.com>
2023-11-08 22:36:33 +0500

    Add authentication buttons to header

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,5 +1,35 @@
 import styled from "styled-components";
 import Link from "./Link";
+import RouterLink from "./RouterLink";
+import HorizontalContainer from "./HorizontalContainer";
+
+const NavLink = styled(Link)`
+  color: #eeeeee;
+  transition: 0.3s;
+
+  &:hover {
+    text-decoration: none;
+    color: #eeeeee;
+    transition: 0.3s;
+  }
+`;
+
+const AuthLink = styled(NavLink)`
+  font-size: 1.3em;
+  padding: 0.5em;
+  border: 2px solid #eeeeee;
+  border-radius: 5px;
+
+  &:hover {
+    background-color: #eeeeee;
+    color: slategray;
+  }
+`;
+
+const AuthLinks = styled(HorizontalContainer)`
+  position: absolute;
+  right: 1em;
+`;
 
 const HeaderContainer = styled.header`
   position: sticky;
@@ -13,18 +43,12 @@ const HeaderContainer = styled.header`
   margin-bottom: 0.5em;
 `;
 
-const LogoText = styled(Link)`
-  color: #eeeeee;
-  margin: 0;
+const LogoText = styled(NavLink)`
   font-weight: bold;
   font-size: 2em;
-  transition: 0.3s;
 
   &:hover {
-    text-decoration: none;
-    color: #eeeeee;
     text-shadow: 5px 5px 10px turquoise;
-    transition: 0.3s;
   }
 `;
 
@@ -32,6 +56,15 @@ const Header = () => {
   return (
     <HeaderContainer>
       <LogoText>Twittur</LogoText>
+
+      <AuthLinks gap="1em">
+        <RouterLink to="/login">
+          <AuthLink>Sign in</AuthLink>
+        </RouterLink>
+        <RouterLink to="/">
+          <AuthLink>Sign up</AuthLink>
+        </RouterLink>
+      </AuthLinks>
     </HeaderContainer>
   );
 };
