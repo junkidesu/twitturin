f73d3d70064283ad6a6019fa5e83849d413588b3
Anwar Sh <alvaro.sh03@gmail.com>
2023-11-18 15:20:01 +0500

    Implement initial tweets reducer

---
+++ frontend/src/reducers/tweetsReducer.ts
@@ -0,0 +1,24 @@
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
+import { Tweet } from "../types";
+import tweetsService from "../services/tweetsService";
+import { AppDispatch } from "../store";
+
+const tweetsSlice = createSlice({
+  name: "tweets",
+  initialState: [] as Tweet[],
+  reducers: {
+    setTweets: (_state, action: PayloadAction<Tweet[]>) => {
+      return action.payload;
+    },
+  },
+});
+
+export default tweetsSlice.reducer;
+
+export const { setTweets } = tweetsSlice.actions;
+
+export const getAllTweets = () => async (dispatch: AppDispatch) => {
+  const tweets: Tweet[] = await tweetsService.getAll();
+
+  dispatch(setTweets(tweets));
+};
--- frontend/src/store.ts
+++ frontend/src/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./reducers/authReducer";
+import tweetsReducer from "./reducers/tweetsReducer";
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
+    tweets: tweetsReducer,
   },
 });
 
