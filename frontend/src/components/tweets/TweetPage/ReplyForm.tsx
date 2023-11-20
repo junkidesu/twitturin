2eb1ce9a6b9175b9d77a7dc4a67758326ec028d4
Anwar Sh <alvaro.sh03@gmail.com>
2023-11-20 21:54:39 +0500

    Show loading spinner modal when leaving a reply

--- frontend/src/components/tweets/TweetPage/ReplyForm.tsx
+++ frontend/src/components/tweets/TweetPage/ReplyForm.tsx
@@ -1,15 +1,28 @@
-import { useAppSelector } from "../../../hooks/store";
+import { useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "../../../hooks/store";
 import useField from "../../../hooks/useField";
 import { useReplyMutation } from "../../../services/repliesService";
 import Button from "../../core/Button";
 import Form from "../../core/Form";
 import TextArea from "../../core/TextArea";
+import Modal from "../../containers/Modal";
+import LoadingSpinner from "../../LoadingSpinner";
+import { hideModal, showModal } from "../../../reducers/modalReducer";
 
 const ReplyForm = ({ id }: { id: string }) => {
-  const [reply] = useReplyMutation();
+  const [reply, { isLoading, isSuccess }] = useReplyMutation();
+  const dispatch = useAppDispatch();
   const content = useField("text", "Type your reply...");
 
   const token = useAppSelector(({ auth }) => auth.token);
+
+  useEffect(() => {
+    if (isLoading) dispatch(showModal());
+  }, [isLoading, dispatch]);
+
+  useEffect(() => {
+    if (isSuccess) dispatch(hideModal());
+  }, [isSuccess, dispatch]);
 
   if (!token) return null;
 
@@ -23,6 +36,10 @@ const ReplyForm = ({ id }: { id: string }) => {
 
   return (
     <Form onSubmit={handleReply}>
+      <Modal>
+        <LoadingSpinner label="Replying to tweet..." />
+      </Modal>
+
       <TextArea {...content} required />
       <Button $bg="white">Submit</Button>
     </Form>
