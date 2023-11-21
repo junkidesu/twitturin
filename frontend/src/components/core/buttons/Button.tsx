db6a1e9a4fdc729db0184ad77976a5781ebe0c8e
Anwar Sh <alvaro.sh03@gmail.com>
2023-11-22 03:34:08 +0500

    Refactor Button component

--- frontend/src/components/core/buttons/Button.tsx
+++ frontend/src/components/core/buttons/Button.tsx
@@ -1,24 +1,28 @@
 import styled from "styled-components";
 
 interface ButtonProps {
+  $size?: "extraSmall" | "small" | "medium" | "large" | "extraLarge";
   $bg?: string;
   $fg?: string;
   $rounded?: boolean;
+  $width?: string;
 }
 
 const Button = styled.button<ButtonProps>`
   background-color: ${(props) => props.$bg || "transparent"};
-  color: ${(props) => props.$fg || props.theme.colors.primary};
-  font-size: ${(props) => props.theme.fontSizes.small};
+  color: ${({ $fg, theme }) => $fg || theme.colors.primary};
+  font-size: ${({ theme, $size }) =>
+    $size ? theme.fontSizes[$size] : theme.fontSizes.small};
   padding: 0.5em;
   border: 3px solid ${(props) => props.$fg || props.theme.colors.primary};
   border-radius: ${(props) => (props.$rounded ? "10em" : "5px")};
   transition: 0.1s;
   cursor: pointer;
+  width: ${({ $width }) => $width || "fit-content"};
 
   &:hover {
-    background-color: ${(props) => props.$fg || props.theme.colors.primary};
-    color: ${(props) => props.$bg || props.theme.colors.background};
+    background-color: ${({ $fg, theme }) => $fg || theme.colors.primary};
+    color: ${({ $bg, theme }) => (!$bg ? theme.colors.background : $bg)};
     transition: 0.1s;
   }
 `;
