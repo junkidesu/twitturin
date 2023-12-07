9767e891cd609602808e8dc00b6b8cb4d4764a03
Anwar Sh <alvaro.sh03@gmail.com>
2023-12-07 15:06:03 +0500

    Implement loading user item component

--- frontend/src/components/users/SuggestedUsers.tsx
+++ frontend/src/components/users/SuggestedUsers.tsx
@@ -2,22 +2,21 @@ import Heading from "../core/text/Heading";
 import Box from "../containers/Box";
 import { useGetUsersQuery } from "../../services/usersService";
 import UserItem from "./UserItem";
+import LoadingUserItem from "../util/LoadingUserItem";
 
 const SuggestedUsers = () => {
   const { data: users, isLoading, isError } = useGetUsersQuery();
 
-  if (isLoading) return <div>loading...</div>;
-
-  if (isError || !users) return <div>Some error occurred!</div>;
+  if (isError) return <div>Some error occurred!</div>;
 
   return (
     <Box $width="350px" $pad="l" $gap="1em" $bg="white" $bradius="1em">
       <Heading $level={3}>Twittur Users</Heading>
 
       <Box>
-        {users.slice(0, 3).map((u) => (
-          <UserItem key={u.id} user={u} />
-        ))}
+        {isLoading || !users
+          ? [1, 2, 3].map(() => <LoadingUserItem />)
+          : users.map((u) => <UserItem key={u.id} user={u} />)}
       </Box>
     </Box>
   );
---
+++ frontend/src/components/util/LoadingUserItem.tsx
@@ -0,0 +1,22 @@
+import LoadingElement from "./LoadingElement";
+import Box from "../containers/Box";
+import styled from "styled-components";
+
+const LoadingProfilePicture = styled(LoadingElement)`
+  border-radius: 10em;
+`;
+
+const LoadingUserItem = () => {
+  return (
+    <Box $horizontal $center $pad="m" $gap="1em">
+      <LoadingProfilePicture $width="2em" $height="2em" />
+
+      <Box $width="80%" $gap="0.5em" $center>
+        <LoadingElement $height="1em" />
+        <LoadingElement $height="1em" />
+      </Box>
+    </Box>
+  );
+};
+
+export default LoadingUserItem;
