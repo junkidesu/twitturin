9d30a47fe0a012c0a7aee38322caa0679a5d01e8
Anwar Sh <alvaro.sh03@gmail.com>
2023-12-17 22:45:44 +0500

    Make header buttons responsive

--- frontend/src/components/Header/AuthButtons.tsx
+++ frontend/src/components/Header/AuthButtons.tsx
@@ -1,12 +1,75 @@
 import { useNavigate } from "react-router-dom";
 import Box from "../containers/Box";
 import HeaderButton from "./HeaderButton";
+import styled from "styled-components";
+import { icons } from "../../assets";
+import IconButton from "../core/buttons/IconButton";
+import Menu from "../core/Menu";
+import HiddenItems from "../core/Menu/HiddenItems";
+import VisibleItems from "../core/Menu/VisibleItems";
+import FlatButton from "../core/buttons/FlatButton";
+import { useState } from "react";
+
+const MenuButton = styled(IconButton).attrs({ icon: <icons.MenuIcon /> })`
+  width: 50px;
+  height: 50px;
+  align-items: center;
+  justify-content: center;
+  color: white;
+  border: 3px solid white;
+  border-radius: 10px;
+`;
+
+const Wrapper = styled(Box)`
+  @media (min-width: 650px) {
+    ${MenuButton} {
+      display: none;
+    }
+  }
+  @media (max-width: 650px) {
+    ${HeaderButton} {
+      display: none;
+    }
+  }
+`;
 
 const AuthButtons = () => {
+  const [visible, setVisible] = useState(false);
   const navigate = useNavigate();
 
+  const onLogin = () => {
+    setVisible(false);
+    navigate("/login");
+  };
+
+  const onSignUp = () => {
+    setVisible(false);
+    navigate("/sign-up");
+  };
+
   return (
-    <Box $horizontal $center $gap="0.5em">
+    <Wrapper $horizontal $center $gap="0.5em">
+      <Menu>
+        <VisibleItems>
+          <MenuButton onClick={() => setVisible(!visible)} />
+        </VisibleItems>
+
+        {visible && (
+          <HiddenItems>
+            <FlatButton
+              icon={<icons.UserCheckIcon />}
+              label="Sign in"
+              onClick={onLogin}
+            />
+            <FlatButton
+              icon={<icons.UserPlusIcon />}
+              label="Sign up"
+              onClick={onSignUp}
+            />
+          </HiddenItems>
+        )}
+      </Menu>
+
       <HeaderButton
         $size="small"
         $bg="transparent"
@@ -23,7 +86,7 @@ const AuthButtons = () => {
       >
         Sign up
       </HeaderButton>
-    </Box>
+    </Wrapper>
   );
 };
 
