09cff9b01ea83781adc0f7f89bc397d41a5e8dd1
Anwar Sh. <alvaro.sh03@gmail.com>
2024-01-30 17:22:48 +0500

    Add docs for search endpoint

--- packages/api/src/routes/search.ts
+++ packages/api/src/routes/search.ts
@@ -5,6 +5,27 @@ import { SearchResults } from "../types";
 
 const router = express.Router();
 
+/**
+ * @openapi
+ * /search:
+ *   get:
+ *     summary: Simple search
+ *     tags: [search]
+ *     parameters:
+ *       - in: query
+ *         name: keyword
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The search keyword (case-insensitive)
+ *     responses:
+ *       200:
+ *         description: The search results
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/SearchData'
+ */
 router.get("/", async (req, res, next) => {
   try {
     const keyword = parseString(req.query.keyword, "keyword");
